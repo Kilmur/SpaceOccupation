02f38c463d8cc7ce60357acea9afc29eb6377d63
Kilmur <miiq@yandex.ru>
2018-02-23 23:16:08 +0300

    Add constructor, get method and field 'level'

--- src/Level.java
+++ src/Level.java
@@ -1,5 +1,15 @@
 
 public enum Level {
-    SingleCellular, MultiCellular, Primitive,
-    Developed, Thinking, Intelligent
+    SingleCellular(0), MultiCellular(1), Primitive(2),
+    Developed(3), Thinking(4), Intelligent(5);
+
+    private int level;
+
+    Level(int level){
+        this.level = level;
+    }
+
+    public int getLevel(){
+        return level;
+    }
 }
