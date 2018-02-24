c0a285cc096ebf5ecf5a82c36918b0f632d67e03
isrusin <is_rusinov@mail.ru>
2018-02-24 18:06:17 +0300

    Update Asteroid constructor

--- src/Asteroid.java
+++ src/Asteroid.java
@@ -1,8 +1,8 @@
 
 public class Asteroid extends Planet {
 
-    public Asteroid(int posX, int posY) {
-        super(posX, posY);
+    public Asteroid(int posX, int posY, long energyGain) {
+        super(posX, posY, energyGain);
     }
 
 }
