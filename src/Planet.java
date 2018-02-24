f71277fc3855d2854fcf8000a493f61db833f8ca
Kilmur <miiq@yandex.ru>
2018-02-24 21:39:37 +0300

    Implement nextStep() for Planet

--- src/Planet.java
+++ src/Planet.java
@@ -18,15 +18,22 @@ public class Planet {
     }
 
     public void nextStep(){
+        int i = 0;
         for(Phase phase: phases){
-            if(!phase.nextStep()){} // Удаление фазы
-
+            if(phase.nextStep()){
+                if(i != (energyPhases.length - 1)){
+                    i++;
+                    energyPhases[i] = phase.energyGainPhase();
+                }else{
+                    energyPhases[i] += phase.energyGainPhase();
+                }
+            }
 
         }
     }
 
-    public void createPhase(){
-
+    public void createPhase(int level, long population){
+        phases.get(level).setPopulation(population);
     }
 
     public long getEnergyForPhase(int level){
