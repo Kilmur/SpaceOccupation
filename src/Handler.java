a7866502be34576d9d1c7edfe2f97e8aff85e63e
isrusin <is_rusinov@mail.ru>
2018-02-28 19:15:48 +0300

    Annotate handlers for XML (un)marshalling

--- src/Handler.java
+++ src/Handler.java
@@ -1,15 +1,41 @@
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlType;
 
 public abstract class Handler<Target> {
     public abstract void apply(Target target);
 }
 
-
+@XmlType
 class ConditionHandler extends Handler<Planet> {
-    int condition;
-    int delta;
+    private int condition;
+    private int delta;
+
+    public ConditionHandler() {}
 
     public ConditionHandler(int condition, int delta) {
         this.condition = condition;
+        this.delta = delta;
+    }
+
+    @XmlAttribute
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    @XmlAttribute
+    public int getDelta() {
+        return delta;
+    }
+
+    public void setDelta(int delta) {
         this.delta = delta;
     }
 
@@ -18,22 +44,38 @@ class ConditionHandler extends Handler<Planet> {
     }
 }
 
-
 abstract class LinkedHandler<Target> extends Handler<Target> {
     Target target;
+    @XmlAttribute @XmlID String id;
 
     public void apply() {
         this.apply(target);
     }
+
+    @SuppressWarnings("unchecked")
+    public void afterUnmarshal(Unmarshaller u, Object parent) {
+        this.target = (Target) parent;
+    }
 }
 
-
+@XmlType
 class PlanetaryDisaster extends LinkedHandler<Planet> {
-    Planet target;
     private Handler<Planet>[] handlers;
+
+    public PlanetaryDisaster() {}
 
     public PlanetaryDisaster(Planet target, Handler<Planet>[] handlers) {
         this.target = target;
+        this.handlers = handlers;
+    }
+
+    @XmlElementWrapper
+    @XmlAnyElement
+    public Handler<Planet>[] getHandlers(){
+        return handlers;
+    }
+
+    public void setHandlers(Handler<Planet>[] handlers) {
         this.handlers = handlers;
     }
 
@@ -45,13 +87,24 @@ class PlanetaryDisaster extends LinkedHandler<Planet> {
     }
 }
 
-
+@XmlType
 class SpaceDisaster extends LinkedHandler<PlanetarySystem> {
-    PlanetarySystem target;
     private Handler<Planet>[] handlers;
+
+    public SpaceDisaster() {}
 
     public SpaceDisaster(PlanetarySystem target, Handler<Planet>[] handlers) {
         this.target = target;
+        this.handlers = handlers;
+    }
+
+    @XmlElementWrapper
+    @XmlAnyElement
+    public Handler<Planet>[] getHandlers(){
+        return handlers;
+    }
+
+    public void setHandlers(Handler<Planet>[] handlers) {
         this.handlers = handlers;
     }
 
