4225c27c76d30d0f26c3c5b3e25d736bc27bf982
isrusin <is_rusinov@mail.ru>
2018-02-28 14:55:28 +0300

    Replace tabs with spaces (Eclipse untuned somehow)

--- src/EventManager.java
+++ src/EventManager.java
@@ -4,45 +4,45 @@ import java.util.stream.Collectors;
 
 
 public class EventManager {
-	List<Event> events;
+    List<Event> events;
 
-	public EventManager() {
-		events = new LinkedList<Event>();
-	}
+    public EventManager() {
+        events = new LinkedList<Event>();
+    }
 
-	public void addEvent(Event event) {
-		events.add(event);
-	}
+    public void addEvent(Event event) {
+        events.add(event);
+    }
 
-	public void addEvent(String name, LinkedHandler<Object> handler, int delay) {
-		events.add(new Event(name, handler, delay));
-	}
+    public void addEvent(String name, LinkedHandler<Object> handler, int delay) {
+        events.add(new Event(name, handler, delay));
+    }
 
-	public void nextStep() {
-		events = events.stream()
-				.filter(wrapper -> wrapper.waitAndApply())
-				.collect(Collectors.toList());
-	}
+    public void nextStep() {
+        events = events.stream()
+                .filter(wrapper -> wrapper.waitAndApply())
+                .collect(Collectors.toList());
+    }
 }
 
 
 class Event {
-	int delay;
-	String name;
-	LinkedHandler<Object> handler;
+    int delay;
+    String name;
+    LinkedHandler<Object> handler;
 
-	public Event(String name, LinkedHandler<Object> handler, int delay) {
-		this.name = name;
-		this.handler = handler;
-		this.delay = delay;
-	}
+    public Event(String name, LinkedHandler<Object> handler, int delay) {
+        this.name = name;
+        this.handler = handler;
+        this.delay = delay;
+    }
 
-	public boolean waitAndApply() {
-		if(delay == 0) {
-			handler.apply();
-			return false;
-		}
-		delay -= 1;
-		return true;
-	}
+    public boolean waitAndApply() {
+        if(delay == 0) {
+            handler.apply();
+            return false;
+        }
+        delay -= 1;
+        return true;
+    }
 }
--- src/Handler.java
+++ src/Handler.java
@@ -1,66 +1,66 @@
 
 public abstract class Handler<Target> {
-	public abstract void apply(Target target);
+    public abstract void apply(Target target);
 }
 
 
-class ConditionHandler extends Handler<Planet>{
-	int condition;
-	int delta;
+class ConditionHandler extends Handler<Planet> {
+    int condition;
+    int delta;
 
-	public ConditionHandler(int condition, int delta) {
-		this.condition = condition;
-		this.delta = delta;
-	}
+    public ConditionHandler(int condition, int delta) {
+        this.condition = condition;
+        this.delta = delta;
+    }
 
-	public void apply(Planet target) {
-		target.conditions[condition] += delta;
-	}
+    public void apply(Planet target) {
+        target.conditions[condition] += delta;
+    }
 }
 
 
 abstract class LinkedHandler<Target> extends Handler<Target> {
-	Target target;
+    Target target;
 
-	public void apply() {
-		this.apply(target);
-	}
+    public void apply() {
+        this.apply(target);
+    }
 }
 
 
 class PlanetaryDisaster extends LinkedHandler<Planet> {
-	Planet target;
-	private Handler<Planet>[] handlers;
+    Planet target;
+    private Handler<Planet>[] handlers;
 
-	public PlanetaryDisaster(Planet target, Handler<Planet>[] handlers) {
-		this.target = target;
-		this.handlers = handlers;
-	}
+    public PlanetaryDisaster(Planet target, Handler<Planet>[] handlers) {
+        this.target = target;
+        this.handlers = handlers;
+    }
 
-	@Override
-	public void apply(Planet target) {
-		for(Handler<Planet> handler : handlers) {
-			handler.apply(target);
-		}
-	}
+    @Override
+    public void apply(Planet target) {
+        for(Handler<Planet> handler : handlers) {
+            handler.apply(target);
+        }
+    }
 }
 
 
 class SpaceDisaster extends LinkedHandler<PlanetarySystem> {
-	PlanetarySystem target;
-	private Handler<Planet>[] handlers;
+    PlanetarySystem target;
+    private Handler<Planet>[] handlers;
 
-	public SpaceDisaster(PlanetarySystem target, Handler<Planet>[] handlers) {
-		this.target = target;
-		this.handlers = handlers;
-	}
+    public SpaceDisaster(PlanetarySystem target, Handler<Planet>[] handlers) {
+        this.target = target;
+        this.handlers = handlers;
+    }
 
-	@Override
-	public void apply(PlanetarySystem target) {
-		for(Handler<Planet> handler : handlers) {
-			for(Planet planet : target.planets) {
-				handler.apply(planet);
-			}
-		}
-	}
+    @Override
+    public void apply(PlanetarySystem target) {
+        for(Handler<Planet> handler : handlers) {
+            for(Planet planet : target.planets) {
+                handler.apply(planet);
+            }
+        }
+    }
 }
