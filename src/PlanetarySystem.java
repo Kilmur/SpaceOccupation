0da890fe67259eb6ad8af9cc7b28664eca13fd33
Max <miiq@yandex.ru>
2019-02-17 09:49:22 +0300

    Add attributes for events

--- src/PlanetarySystem.java
+++ src/PlanetarySystem.java
@@ -5,11 +5,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class PlanetarySystem {
 
-    Planet[] planets;
-    ArrayList<Planet> carriers;
-    EventManager manager;
+  Planet[] planets;
+  ArrayList<Planet> carriers;
+  // События, возможные для данной звездной системы
+  ArrayList<Event> availableEvents;
+  // События, действующие в данный момент в звездной системе
+  ArrayList<Event> actualEvents;
 
-    public void nextStep(){
-    }
+  public void nextStep () {
+    
+  }
 
 }
